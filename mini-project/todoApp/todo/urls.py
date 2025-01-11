538f06b582213f78ca9a6f1b17338025783d8db8
chetanmelavanki <chetanmmelavanki>
2025-01-11 19:30:37 +0530

    add task and delete task

--- mini-project/todoApp/todo/urls.py
+++ mini-project/todoApp/todo/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('addtask/',views.add_task,name='addtask'),
-    path('tasklist/',views.Task_view,name='task_list')
+    path('',views.add_task,name='addtask'),
+    path('tasklist/',views.Task_view,name='task_list'),
+    path('delete/<int:task_id>/',views.Delete_task,name='delete_task'),
+    path('complete/<int:task_id>/',views.Completed_task,name='complete_task'),
+
 ]
--- mini-project/todoApp/todo/views.py
+++ mini-project/todoApp/todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Todo
 from django.http import HttpResponse
 
@@ -7,11 +7,27 @@ def home(request):
 
 def Task_view(request):
     all_task = Todo.objects.all()
-    return render(request,'task_list.html',{'task':all_task})
+    return render(request, 'task_list.html', {'task': all_task})
 
 def add_task(request):
-    if request.method=='POST':
-        title = request.POST['title']
-        Todo.objects.create(title=title)
-        return redirect('task_list')
-    return render(request,'addtask.html')
+    if request.method == 'POST':
+        title = request.POST.get('title')  # Get the title from the form
+        if title:  # Ensure the title is not empty
+            Todo.objects.create(title=title)  # Add the task to the database
+    # Fetch the updated task list after adding
+    all_task = Todo.objects.all()
+    return render(request, 'addtask.html', {'task': all_task})
+
+
+def Completed_task(request, task_id):
+    
+    task = get_object_or_404(Todo, id=task_id)
+    task.completed = True
+    task.save()
+    return redirect('task_list')  
+
+def Delete_task(request, task_id):
+    
+    task = get_object_or_404(Todo, id=task_id)
+    task.delete()
+    return redirect('task_list')  
